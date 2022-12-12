af5b3c936929ebda4d225c55256065058eaec1aa
Anderford <incoqwert@gmail.com>
2022-12-12 22:31:57 +0300

    task 5.2.2 - 5.2.3

--- Program.cs
+++ Program.cs
@@ -3,9 +3,23 @@
 class MainClass
 {
 
-    static string? ShowColor()
+    static string? ShowColor(string UserName)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        (string UserName, int age) anketa;
+
+        /*Console.Write("Введите имя: ");
+        anketa.UserName = Console.ReadLine();
+        Console.Write("Введите возраст с цифрами: ");
+        anketa.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ваше имя: {0}", anketa.UserName);
+        Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
+        Console.ReadKey();*/
+
+
+        Console.WriteLine($"Привет {UserName} напишите свой любимый цвет на английском с маленькой буквы");
         var color = Console.ReadLine();
         //return color;
         switch (color)
@@ -40,7 +54,6 @@ class MainClass
 
     public static void Main(string[] args)
     {
-
         var (name, age) = ("Евгения", 27);
 
         Console.WriteLine("Мое имя: {0}", name);
@@ -53,12 +66,14 @@ class MainClass
 
         Console.WriteLine("Ваше имя: {0}", name);
         Console.WriteLine("Ваш возраст: {0}", age);
-        for (int i = 0; i < 4; i++)
+
+        var favcolor = new string?[3];
+        for (int i = 0; i < favcolor.Length; i++)
         {
-            ShowColor();
+            favcolor[i] = ShowColor(name);
         }
         GetArrayFromConsole();
-        
+     
 
     }
     static int[] GetArrayFromConsole()
@@ -71,6 +86,8 @@ class MainClass
             result[i] = int.Parse(Console.ReadLine());
         }
         int temp;
+
+        
         for(int i = 0; i < result.Length; i++)
         {
             for(int j = 0; j < result.Length; j++)
