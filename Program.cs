60e84ce1fa180ee981388753109d5ad6d5a5be07
Anderford <incoqwert@gmail.com>
2022-12-13 23:43:37 +0300

    task 5.3.13

--- Program.cs
+++ Program.cs
@@ -44,7 +44,11 @@ class MainClass
 
     public static void Main(string[] args)
     {
+
         
+        var array = GetArrayFromConsole();
+
+        SortArray(array, out int[] sortedasc, out int[] sorteddesc);
 
         var SomeName = "Ilia";
         Console.WriteLine(SomeName);
@@ -57,6 +61,73 @@ class MainClass
         BigDataOperation(arr);
 
         Console.WriteLine(arr[0]);
+
+    }
+    static int[] GetArrayFromConsole()
+    {
+        var result = new int[5];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+        return result;
+    }
+    static void SortArray(int[] array, out int[] sortedasc, out int[] sorteddesc)
+    {
+        sortedasc = new int[array.Length];
+        sorteddesc = new int[array.Length];
+
+        SortArrayAsc(array).CopyTo(sortedasc, 0);
+        Console.WriteLine("");
+        SortArrayDesc(array).CopyTo(sorteddesc, 0);
+    }
+
+    static int[] SortArrayAsc(int[] result)
+    {
+        int temp;
+        for (int i = 0; i < result.Length; i++)
+        {
+            for (int j = 0; j < result.Length; j++)
+            {
+                if (result[i] < result[j])
+                {
+                    temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+            }
+        }
+        foreach (var item in result)
+        {
+            Console.Write(item + " ");
+        }
+        return result;
+
+    }
+
+    static int[] SortArrayDesc(int[] result)
+    {
+        int temp;
+        for (int i = 0; i < result.Length; i++)
+        {
+            for (int j = 0; j < result.Length; j++)
+            {
+                if (result[i] > result[j])
+                {
+                    temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+            }
+        }
+        foreach (var item in result)
+        {
+            Console.Write(item + " ");
+        }
+
+        return result;
 
     }
 
